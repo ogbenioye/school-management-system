6603eeb8558888b4dee6ccdae7c4f2f3af9d8b85
Oluwapelumi Oyenuga <oluwapelumioyenuga@gmail.com>
2023-03-12 12:18:16 +0100

    new feat: enrol students

--- src/handler/student.go
+++ src/handler/student.go
@@ -1,1 +1,25 @@
-package controller
+package handler
+
+import (
+	"net/http"
+	"school-management-system/src/models"
+	"school-management-system/src/utils"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
+)
+
+func EnrolStudent(c *gin.Context) {
+	student := &models.Student{}
+
+	utils.BindJSON(student, c)
+	id := xid.New()
+	student.StudentID = "STU/" + strconv.Itoa(id.Time().Year()) + "/" + strconv.Itoa(int(id.Counter()))
+	student.EnrolledAt = time.Now()
+
+	s := student.EnrolStudent()
+
+	c.JSON(http.StatusOK, s)
+}
--- src/utils/utils.go
+++ src/utils/utils.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func BindJSON(student models.Student, c *gin.Context) {
+func BindJSON(student *models.Student, c *gin.Context) {
 	if err := c.ShouldBindJSON(&student); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"Error": err.Error(),
