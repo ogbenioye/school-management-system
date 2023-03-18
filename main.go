055fcfa43bb4b4f5c53e67b51ecbf6ebb4fec5bb
Oluwapelumi Oyenuga <oluwapelumioyenuga@gmail.com>
2023-03-18 18:26:24 +0100

    documentint api endpoints

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
+// School Management System API
+//
+// Schemes: http
+// Host: localhost:8080
+// BasePath: /
+// Version: 1.0.0
+// Contact: Oluwapelumi Oyenuga<oluwapelumioyenuga@gmail.com>
+//
+// Consumes:
+// - application/json
+//
+// Produces:
+// - application/json
+//
+// swagger:meta
 package main
 
 import (
